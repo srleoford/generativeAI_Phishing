bc12acef957f41cbbaae10d28873d8d27a4ad742
Blanca A Cuellar <blancac@Blancas-MBP.lan>
2024-10-28 14:14:26 -0600

    modified user exist check

--- src/app/utils/pinecone.ts
+++ src/app/utils/pinecone.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { Pinecone } from "@pinecone-database/pinecone";
+import { redirect } from 'next/navigation';
 
 // Default values for Pinecone such as the default vector for user creation, Pinecone API, etc.
 const defaultVector = [0,1,2,3,3,2,1,2,3,3,2,3,2,1,2,3,2,3,2]
@@ -38,6 +39,22 @@ const hasNamespace = async (index: string, namespace: string) => {
     const { namespaces } = await pc.index(index).describeIndexStats()
     return namespaces.hasOwnProperty(namespace)
 }
+/**
+ * This checks if a user with the given userEmail exists in a specified index.
+ * @param index
+ * @param userEmail
+ * @returns resolves to true if the user exists, otherwise false.
+ */
+export const userExists = async (index: string, userEmail: string): Promise<boolean> => {
+    try {
+        // Fetch the user by email from the specified index
+        const result = await pc.index(index).fetch([userEmail]);
+        return result && result.records[userEmail] !== undefined;
+    } catch (error) {
+        console.error(`Error checking user existence by user email in index ${index}:`, error);
+        return false;
+    }
+};
 
 /**
  * This will upsert into Pinecone DB for new users. The namespace will be the user's email. This requires to be vectors
@@ -63,7 +80,7 @@ export const insertUser = async(userEmail: string, token: string) =>{
                 metadata: { email: userEmail, token: token }
             }
         ]
-        if (await hasNamespace(indexName, userEmail)) {
+        if (await userExists(indexName, userEmail)) {
             return redirect("/declinedSurvey");
         }
         // This needs to check if the user exists before inserting. If not, `redirect("/declinedSurvey")` or some
